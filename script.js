75d1fc98740db933a3ed972932f188015f76169f
Josh Waters <Jm.joshmoll@gmail.com>
2025-04-03 07:33:57 +0100

    Added FEN display

--- script.js
+++ script.js
@@ -28,7 +28,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
             for (let j = 0; j < row.length; j++) {
                 const piece = row[j];
-
                 if (!isNaN(piece)) {
                     squareIndex += parseInt(piece);
                 } else {
@@ -46,7 +45,58 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 
+    function currentFen() {
+        let currentFen = '';
+    
+        for (let i = 0; i < 8; i++) {
+            let emptySpaces = 0;
+    
+            for (let j = 0; j < 8; j++) {
+                const square = document.getElementsByClassName('square')[i * 8 + j];
+                const piece = square.getElementsByClassName('piece')[0];
+    
+                if (piece) {
+                    if (emptySpaces > 0) {
+                        currentFen += emptySpaces;
+                        emptySpaces = 0;
+                    }
+                    currentFen += piece.classList[1].split('-')[1];
+                } else {
+                    emptySpaces += 1;
+                }
+            }
+    
+            if (emptySpaces > 0) {
+                currentFen += emptySpaces;
+            }
+    
+            if (i < 7) {
+                currentFen += '/';
+            }
+        }
+        
+        document.getElementById("fen").innerHTML = currentFen;
+        //alert(currentFen);
+    
+        // Turn / castling / en passant / halfmove / fullmove
+    }
+
+    function copyFen() {
+        const fen = document.getElementById("fen").innerHTML;
+        const textArea = document.createElement("textarea");
+        textArea.value = fen;
+        document.body.appendChild(textArea);
+        textArea.select();
+        document.execCommand("Copy");
+        textArea.remove();
+        alert("FEN copied: " + fen);
+    }
+
     createChessboard()
     const initialFEN = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR';
     updateChessboard(initialFEN);
+    currentFen();
+
+    document.getElementById("shareButton").addEventListener("click", copyFen);
 });
+
